02fdbf13be7861b7678f6cb10b88cd93494022a4
durgasaikalyan <durgasaikalyanccbp@gmail.com>
2022-12-08 17:52:14 +0000

    your message

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+    // Write your code here.
+    <div>
+        <h1 Classname = "greeting">Congratulations</h1>
+        <p Classname = "desceiptive">Kiran V</p> 
+        <img src = "https://assets.ccbp.in/frontend/react-js/congrats-card-bg.png" Classname = "image1"
+        />
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
